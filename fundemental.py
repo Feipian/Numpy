70a0ecc55ac13716f9a3e996b57841171de1f853
HongMing <superwang0603@gmail.com>
2024-02-16 10:58:42 +0800

    Learn numpy!!

---
+++ fundemental.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1,2,3], dtype='int64')
+
+b = np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+print(b)
+
+print(a.ndim)
+print(a.shape)
+
+print(a.dtype)
+print(a.itemsize)
+
+print(a.size)
+
+
+
